dadf09b48cf23c6c1d1804c72aa598b5ff0d9f14
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2026-05-29 19:33:12 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReportEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
